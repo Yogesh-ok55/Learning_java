5b6b98601e1422556eab2484384e9c5d3f46e3c5
Yogesh Choudhary <yogesh986.be22@chitkara.edu.in>
2024-07-19 17:26:25 +0530

    adding info of input object

--- intro.java
+++ intro.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class intro{
     public static void main(String [] args){
-        System.out.print("hello");
+        System.out.println("hello");
+
+        System.out.print("Enter your name: ");
+
+
+
+        Scanner scanner = new Scanner(System.in);//creating object of Scanner class important
+        // to take input 
+
+
+
+        String str= scanner.nextLine();
+        //taking input using scanner for string
+
+        // int num= scanner.nextInt();
+        //taking input using scanner for int
+
+        // float marks= scanner.nextFloat();
+        //taking input using scanner for float
+
+        
+
+
+        String name="Welcome to vs code";
+        System.out.println(name+" "+str);
+
+        
+
     }
 }
